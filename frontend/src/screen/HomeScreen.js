17d3cece209f2fe5e10721cf68d236b228def3c4
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-02-10 21:10:36 +0530

    Home screen

---
+++ frontend/src/screen/HomeScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './css/Home.css';
+
+const HomeScreen = () => {
+    return (
+        <div className='homescreen__container'>
+            <h1>MERN 🔥</h1>
+            <h2>login management</h2>
+        </div>
+    );
+};
+
+export default HomeScreen;
