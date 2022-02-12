3dae2a3f88d2dbf6b28ba7232503ef2771bb5ef9
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-02-12 11:16:52 +0530

    Action types

--- frontend/src/redux/actions/types.js
+++ frontend/src/redux/actions/types.js
@@ -13,3 +13,9 @@ export const AUTH_ERROR = "AUTH_ERROR";
 export const GET_ERROR = "GET_ERROR";
 export const CLEAR_ERRORS = "CLEAR_ERRORS";
 
+
+export const ADD_ITEM = "ADD_ITEM";
+export const GET_ITEMS = "GET_ITEMS";
+export const ITEM_CHECK = "ITEM_CHECK";
+export const DELETE_ITEM = "DELETE_ITEM";
+export const ITEMS_LOADING = "ITEMS_LOADING";
