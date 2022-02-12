df7219a3ff0eb9901d933ca88ff4dc55cf744ebc
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-02-12 11:18:50 +0530

    Reducer

--- frontend/src/redux/reducers/index.js
+++ frontend/src/redux/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import { authReducer } from './authReducer';
 import { errorReducer } from './errorReducer';
+import { itemReducer } from './itemReducer';
 
 
 export const reducer = combineReducers({
     auth: authReducer,
+    item: itemReducer,
     error: errorReducer
 });
 
