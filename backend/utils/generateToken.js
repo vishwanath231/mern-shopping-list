27e06d798825f4c8e91f440953bb1af7eb759d31
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-02-12 15:22:24 +0530

    Generate token

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 
 const generateToken = (id) => {
 
-    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d'})
+    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '24h'})
 }
 
 export default generateToken;
