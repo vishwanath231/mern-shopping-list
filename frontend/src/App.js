3c32ba600293f0e2db9a1fbb6df17a1873e81f39
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-02-12 14:44:09 +0530

    React app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React,{ useEffect } from 'react';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import Navbar from './components/navbar/Navbar';
 import Home from './screen/Home';
 import LoginScreen from './screen/LoginScreen';
 import RegisterScreen from './screen/RegisterScreen';
