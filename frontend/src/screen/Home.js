6c93e53aaa47580457410abb3a40e2ee4d6b12d8
vishwanath231 <vishwanathvishwabai@gmail.com>
2022-02-12 11:23:50 +0530

    Home

--- frontend/src/screen/Home.js
+++ frontend/src/screen/Home.js
@@ -1,21 +1,18 @@
 import React from 'react';
+import NewItem from './NewItem';
 import './css/Home.css';
-import { connect } from 'react-redux';
-import Dashboard from './DashboardScreen';
 import HomeScreen from './HomeScreen';
 
-const Home = ({ auth }) => {
 
+const Home = () => {
     return (
         <>
-            {auth.isAuthenticated ? <Dashboard auth={auth} /> : <HomeScreen />}
+            <NewItem />
+            <HomeScreen />
         </>
     );
 };
 
-const mapStateToProps = (state) => ({
-    auth: state.auth
-})
 
-export default connect(mapStateToProps, null)(Home);
+export default Home;
 
